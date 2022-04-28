0d0e31e184faaf0faaccd634b22b82badfe57e3b
Sylvie-B <unsolo@hotmail.fr>
2022-04-28 11:52:57 +0200

    setting bootstrap

---
+++ View/partials/footer.php
@@ -0,0 +1,8 @@
+<nav class="navbar fixed-bottom navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Makers Fourmies</a>
+    </div>
+</nav>
+    <script src="/assets/app.js"></script>
+</body>
+</html>
