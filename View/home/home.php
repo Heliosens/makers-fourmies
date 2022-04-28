5dca7289f0d665da8774bdceea8e8b8875e8710c
Sylvie-B <unsolo@hotmail.fr>
2022-04-28 16:00:17 +0200

    fix bootstrap js redo header

--- View/home/home.php
+++ View/home/home.php
@@ -1,3 +1,25 @@
 <section id="band">
     <h1>MAKERS FOURMIES</h1>
 </section>
+<section class="frame d-flex justify-content-around">
+    <div>
+        <div class="border border-secondary">
+
+        </div>
+    </div>
+    <div>
+        <div class="border border-secondary">
+
+        </div>
+    </div>
+    <div>
+        <div class="border border-secondary">
+
+        </div>
+    </div>
+    <div>
+        <div class="border border-secondary">
+
+        </div>
+    </div>
+</section>
--- View/partials/header.php
+++ View/partials/header.php
@@ -16,11 +16,10 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">MAKERS FOURMIES</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -28,16 +27,18 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Ressources</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Projets</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Projets
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Impression 3D</a></li>
+                            <li><a class="dropdown-item" href="#">Modélisation</a></li>
+                            <li><a class="dropdown-item" href="#">...</a></li>
+                        </ul>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fa-solid fa-user"></i>
-                        </a>
                     </li>
                 </ul>
             </div>
