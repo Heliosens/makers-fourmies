88b930d70659387607abbff34d48e8fb431220b3
Sylvie-B <unsolo@hotmail.fr>
2022-04-28 13:50:21 +0200

    setting footer

--- View/partials/footer.php
+++ View/partials/footer.php
@@ -1,13 +1,16 @@
     <footer>
-        <nav class="fixed-bottom bg-light">
-            <ul class="nav justify-content-center">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
+        <nav class="navbar fixed-bottom navbar-expand-lg navbar-light bg-light ">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Contact</a>
+                    </div>
+                </div>
+            </div>
         </nav>
     </footer>
     <script src="/assets/app.js"></script>
