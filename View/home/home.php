2eec539793148fcb5a66f4599a836d6045ffa2ee
Sylvie-B <unsolo@hotmail.fr>
2022-04-28 17:33:36 +0200

    home links

--- View/home/home.php
+++ View/home/home.php
@@ -3,52 +3,48 @@
 </section>
 <section class="frame mx-auto d-flex flex-wrap justify-content-around">
     <div>
-        <div>
-            <div class="card shadow">
-                <h5 class="card-header">Maker</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Qu'est ce donc ?</h5>
-                    <p class="card-text">Le mouvement maker ?<br>Les maker faire ?<br>Les fablabs ?</p>
-                    <a href="#" class="btn btn-primary">En savoir plus</a>
-                </div>
+        <div class="card shadow">
+            <a href="" class="text-decoration-none card-header">
+                <h5  class="text-dark" >Maker</h5>
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">Qu'est ce donc ?</h5>
+                <p class="card-text">Le mouvement maker ?<br>Les maker faire ?<br>Les fablabs ?</p>
             </div>
         </div>
     </div>
     <div>
-        <div>
-            <div class="card shadow">
-                <h5 class="card-header">Ressources</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Les liens utiles</h5>
-                    <p class="card-text">Les logiels gratuits<br>Les applications en ligne<br>Les formations</p>
-                    <a href="#" class="btn btn-primary">En savoir plus</a>
-                </div>
+        <div class="card shadow">
+            <a href="" class="text-decoration-none card-header">
+                <h5 class="text-dark">Ressources</h5>
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">Les liens utiles</h5>
+                <p class="card-text">Les logiels gratuits<br>Les applications en ligne<br>Les formations</p>
             </div>
         </div>
     </div>
     <div>
-        <div>
-            <div class="card shadow">
-                <h5 class="card-header">Projets</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Réalisations de makers</h5>
-                    <p class="card-text">Retrouvez les projets<br>Fiche technique et images<br>
-                        Partagez vos créations</p>
-                    <a href="#" class="btn btn-primary">Voir les catégories</a>
-                </div>
+        <div class="card shadow">
+            <a href="" class="text-decoration-none card-header">
+                <h5 class="text-dark">Projets</h5>
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">Réalisations de makers</h5>
+                <p class="card-text">Retrouvez les projets<br>Fiche technique et images<br>
+                    Partagez vos créations</p>
             </div>
         </div>
     </div>
     <div>
-        <div>
-            <div class="card shadow">
-                <h5 class="card-header">Contact</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Une question ?</h5>
-                    <p class="card-text">Contactez nous pour une question ou un message sur les makers,
-                        un avis sur le site...</p>
-                    <a href="#" class="btn btn-primary">Envoyer un message</a>
-                </div>
+        <div class="card shadow">
+            <a href="" class="text-decoration-none card-header">
+                <h5 class="text-dark">Contact</h5>
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">Une question ?</h5>
+                <p class="card-text">Contactez nous pour une question ou un message sur les makers,
+                    un avis sur le site...</p>
             </div>
         </div>
     </div>
