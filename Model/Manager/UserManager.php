f899181222a1f391104bc12c11c33e923a9e5344
Sylvie-B <unsolo@hotmail.fr>
2022-05-03 15:24:41 +0200

    mail ever exist ?

--- Model/Manager/UserManager.php
+++ Model/Manager/UserManager.php
@@ -3,5 +3,13 @@
 
 class UserManager
 {
-
+    /**
+     * check if email ever exist
+     * @param string $email
+     * @return mixed
+     */
+    public static function mailEverExist (string $email){
+        $nbr = DB::getConn()->query("SELECT count(*) FROM mkf_user WHERE mail = '$email'");
+        return $nbr->fetch();
+    }
 }
