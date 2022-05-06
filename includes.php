e0ed6be4a36993845174c98c4d1a67a86b7d413f
Sylvie-B <unsolo@hotmail.fr>
2022-05-06 17:58:10 +0200

    includes

--- includes.php
+++ includes.php
@@ -1,16 +1,30 @@
 <?php
-
+// connection class
 require __DIR__ . '/Model/DB.php';
 
+// Entity class
 require __DIR__ . '/Model/Entity/Technique.php';
 require __DIR__ . '/Model/Entity/Article.php';
 require __DIR__ . '/Model/Entity/Category.php';
 require __DIR__ . '/Model/Entity/User.php';
 require __DIR__ . '/Model/Entity/Avatar.php';
 require __DIR__ . '/Model/Entity/Role.php';
+require __DIR__ . '/Model/Entity/Type.php';
+require __DIR__ . '/Model/Entity/Image.php';
+require __DIR__ . '/Model/Entity/Matter.php';
+require __DIR__ . '/Model/Entity/Own_category.php';
+require __DIR__ . '/Model/Entity/Resource.php';
+require __DIR__ . '/Model/Entity/Take_part.php';
+require __DIR__ . '/Model/Entity/Tool.php';
+require __DIR__ . '/Model/Entity/Use_matter.php';
+require __DIR__ . '/Model/Entity/Use_resource.php';
+require __DIR__ . '/Model/Entity/Use_tech.php';
+require __DIR__ . '/Model/Entity/Use_tool.php';
 
+// router class
 require __DIR__ . '/Routing/Router.php';
 
+// controller class
 require __DIR__ . '/Controller/Controller.php';
 require __DIR__ . '/Controller/HomeController.php';
 require __DIR__ . '/Controller/MakerController.php';
@@ -20,6 +34,7 @@ require __DIR__ . '/Controller/ContactController.php';
 require __DIR__ . '/Controller/UserController.php';
 require __DIR__ . '/Controller/ProfileController.php';
 
+// Manager class
 require __DIR__ . '/Model/Manager/TechnicManager.php';
 require __DIR__ . '/Model/Manager/CategoryManager.php';
 require __DIR__ . '/Model/Manager/ProjectsManager.php';
