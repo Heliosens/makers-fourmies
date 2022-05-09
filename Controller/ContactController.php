7f02278a75991b8cf8f8e119da7313a0fba8c628
Sylvie-B <unsolo@hotmail.fr>
2022-05-09 11:33:46 +0200

    send mail

--- Controller/ContactController.php
+++ Controller/ContactController.php
@@ -3,7 +3,26 @@
 
 class ContactController extends Controller
 {
+    /**
+     * display contact form
+     */
     public function page(){
         $this->render('contact');
+    }
+
+    /**
+     * send mail from user to mkf
+     */
+    public function send_mail(){
+        if(isset($_POST['sendBtn'], $_POST['user-mail'], $_POST['subject'], $_POST['user-message'])){
+            $from = $this->cleanEntries($_POST['user-mail']);
+            $subject = $this->cleanEntries($_POST['subject']);
+            $txt = $this->cleanEntries($_POST['user-message']);
+            $txt = wordwrap($txt, 70, "/r/n");
+            $headers = array(
+                'X-Mailer' => 'PHP/' . phpversion()
+            );
+            echo mail('makers.fourmies@gmail.com', $subject, $txt, $headers, $from);
+        }
     }
 }
--- View/contact.php
+++ View/contact.php
@@ -2,11 +2,17 @@
     <section class="py-4">
         <div class="mx-auto pb-3 row col-10 col-sm-7 col-md-4">
             <h2 class="pt-3 text-center">Contact</h2>
-            <form action="">
+            <form action="index.php?c=contact&p=send_mail" method="post">
                 <div>
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput">
-                        <label for="floatingInput">Email</label>
+                        <input type="email" name="user-mail" class="form-control" id="floatingInput1">
+                        <label for="floatingInput1">Email</label>
+                    </div>
+                </div>
+                <div>
+                    <div class="form-floating mb-3">
+                        <input type="text" name="subject" class="form-control" id="floatingInput2">
+                        <label for="floatingInput2">Sujet</label>
                     </div>
                 </div>
                 <div>
