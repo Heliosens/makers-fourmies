d58b8f7ccb0c031b121b8777249aa3fae2f6a20d
Sylvie-B <unsolo@hotmail.fr>
2022-05-14 00:01:37 +0200

    traduction in config

--- Config.php
+++ Config.php
@@ -11,6 +11,45 @@ class Config
     public const USER = "root";
     public const PASSWORD = "";
 
+    /**
+     * @param $param
+     * @return string
+     */
+    public static function roleName($param){
+        switch ($param){
+            case 'admin' :
+                return'Administrateur';
+                break;
+            case 'modo' :
+                return 'Modérateur';
+                break;
+            case 'user' :
+                return 'Utilisateur';
+                break;
+            default :
+                return 'Unknown';
+        }
+    }
+
+    /**
+     * @param $param
+     * @return string|null
+     */
+    public static function stateName($param){
+        switch ($param){
+            case 1 :
+                return 'Privé';
+                break;
+            case 2 :
+                return 'En attente de validation';
+                break;
+            case 3 :
+                return 'Publié';
+                break;
+            default :
+                return null;
+        }
+    }
 }
 
 
--- Controller/Controller.php
+++ Controller/Controller.php
@@ -39,46 +39,6 @@ class Controller
     }
 
     /**
-     * @param $param
-     * @return string
-     */
-    public static function roleName($param){
-        switch ($param){
-            case 'admin' :
-                return'Administrateur';
-                break;
-            case 'modo' :
-                return 'Modérateur';
-                break;
-            case 'user' :
-                return 'Utilisateur';
-                break;
-            default :
-                return 'Unknown';
-        }
-    }
-
-    /**
-     * @param $param
-     * @return string|null
-     */
-    public static function stateName($param){
-        switch ($param){
-            case 1 :
-                return 'Privé';
-                break;
-            case 2 :
-                return 'En attente de validation';
-                break;
-            case 3 :
-                return 'Publié';
-                break;
-            default :
-                return null;
-        }
-    }
-
-    /**
      * test if there's one admin at least
      * @return bool
      */
