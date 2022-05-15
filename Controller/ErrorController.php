16fc014c1558cdb789bd70328c1142a34608ec83
Sylvie-B <unsolo@hotmail.fr>
2022-05-15 15:56:33 +0200

    page 404

---
+++ Controller/ErrorController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class ErrorController
+{
+    /**
+     * error page
+     */
+    public function page (){
+        $tech = TechnicManager::getAllKeyName('technique');
+        require __DIR__ . '/../View/partials/header.php';
+        require __DIR__ . '/../View/page404.php';
+        require __DIR__ . '/../View/partials/footer.php';
+    }
+
+}
---
+++ View/page404.php
@@ -0,0 +1,7 @@
+<main>
+    <section class="pt-5">
+        <div class="mx-auto pb-3 row text-center">
+            <h1>Page 404</h1>
+        </div>
+    </section>
+</main>
