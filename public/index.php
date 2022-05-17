9a310a56ef2aab1918a6231a08c335da13640548
Sylvie-B <unsolo@hotmail.fr>
2022-05-17 15:34:04 +0200

    get option

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../includes.php';
 
 $ctrl = isset($_GET['c']) ? Router::cleanParam($_GET['c']) : 'home';
 $param = isset($_GET['p']) ? Router::cleanParam($_GET['p']) : 'page';
-$option = isset($_GET['o']) ? Router::cleanParam($_GET['o']) : null;
+$option = isset($_GET['o']) ? Router::cleanParam($_GET['o']) : '0';
 
 $route = new Router();
 $route->toCtrl($ctrl, $param, $option);
