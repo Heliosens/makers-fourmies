e6c9413e633b061020b92d440b9c123b71d706fd
Sylvie-B <unsolo@hotmail.fr>
2022-05-23 09:10:13 +0200

    work on rubric

---
+++ View/rubric_form.php
@@ -0,0 +1,39 @@
+<main>
+    <section class="pt-5">
+        <div class="mx-auto pb-3 row text-center">
+            <div>
+                <h2>Rubrique</h2>
+                <div>
+                    <div class="m-3">
+                        <form action="/index.php?c=articles&p=add_rubric&o=1">
+                            <label for="">Ajouter un type : </label>
+                            <input type="text">
+                            <input class="btn btn-success btn-sm" value="valider" type="submit">
+                        </form>
+                    </div>
+                    <div class="m-3">
+                        <form action="/index.php?c=articles&p=add_rubric&o=2">
+                            <label for="">Ajouter une categorie : </label>
+                            <input type="text">
+                            <input class="btn btn-success btn-sm" value="valider" type="submit">
+                        </form>
+                    </div>
+                    <div class="m-3">
+                        <form action="/index.php?c=articles&p=add_rubric&o=3">
+                            <label for="">Ajouter une technique : </label>
+                            <input type="text">
+                            <input class="btn btn-success btn-sm" value="valider" type="submit">
+                        </form>
+                    </div>
+                    <div class="m-3">
+                        <form action="/index.php?c=articles&p=add_rubric&o=4">
+                            <label for="">Ajouter une ressource : </label>
+                            <input type="text">
+                            <input class="btn btn-success btn-sm" value="valider" type="submit">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
